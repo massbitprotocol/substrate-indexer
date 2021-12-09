9ad4415075aeb63a877fa784f47c50161ad6ad17
datzero9 <nguyenmanhdat2903@gmail.com>
2021-12-09 23:21:54 +0700

    Fix Dockerfile

--- packages/manager/src/indexer/workers/build.js
+++ packages/manager/src/indexer/workers/build.js
@@ -2,7 +2,7 @@ const {execSync} = require('child_process');
 const {parentPort} = require('worker_threads');
 
 parentPort.on('message', (param) => {
-  execSync('npm install', {cwd: param, stdio: 'ignore'});
-  execSync('npm run build', {cwd: param, stdio: 'ignore'});
+  execSync('npm install', {cwd: param, stdio: 'inherit'});
+  execSync('npm run build', {cwd: param, stdio: 'inherit'});
   parentPort.postMessage(true);
 });
